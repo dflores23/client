04b206bd9a7ed1f4ca95c31bb8233463501fb2f5
David Flores <davidafloresvel@gmail.com>
2022-02-02 06:05:22 -0700

    4th

--- src/application.tsx
+++ src/application.tsx
@@ -1,22 +1,96 @@
-import React from 'react'
-import { Route, RouteChildrenProps, Switch } from 'react-router-dom';
+import React, { useEffect, useReducer, useState } from 'react';
+import { Route, RouteComponentProps, Switch } from 'react-router';
+import { RouteChildrenProps } from 'react-router-dom';
+import AuthRoute from './components/AuthRoute';
+import LoadingComponent from './components/LoadingComponent';
+import logging from './config/logging';
 import routes from './config/routes';
+import { initialUserState, UserContextProvider, userReducer } from './contexts/user';
+import { Validate } from './modules/Auth';
+
 
 export interface IApplicationProps {}
- 
-const Application: React.FunctionComponent<IApplicationProps> = props => {
+
+const Application: React.FunctionComponent<IApplicationProps> = (props) => {
+    const [userState, userDispatch] = useReducer(userReducer, initialUserState);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [authStage, setAuthStage] = useState<string>('Checking localstorage ...');
+
+    useEffect(() => {
+        setTimeout(() => {
+            CheckLocalStorageForCredentials();
+        }, 1000);
+    }, []);
+
+    const CheckLocalStorageForCredentials = () => {
+        setAuthStage('Checking credentials ...');
+
+        const fire_token = localStorage.getItem('fire_token');
+
+        if (fire_token === null) {
+            userDispatch({ type: 'logout', payload: initialUserState });
+            setAuthStage('No credentials found');
+            setTimeout(() => {
+                setLoading(false);
+            }, 500);
+        } else {
+            return Validate(fire_token, (error: any, user: any) => {
+                if (error) {
+                    logging.error(error);
+                    setAuthStage('Invalid credentials');
+                    userDispatch({ type: 'logout', payload: initialUserState });
+                    setTimeout(() => {
+                        setLoading(false);
+                    }, 1000);
+                } else if (user) {
+                    setAuthStage('Valid credentials');
+                    userDispatch({ type: 'login', payload: { user, fire_token } });
+                    setTimeout(() => {
+                        setLoading(false);
+                    }, 1000);
+                }
+            });
+        }
+    };
+
+    const userContextValues = {
+        userState,
+        userDispatch
+    };
+
+    if (loading) {
+        return <LoadingComponent>{authStage}</LoadingComponent>;
+    }
+
     return (
-        <Switch>
-        {routes.map((route, index) => (
-            <Route
-            key={index}
-            path={route.path}
-            exact={route.exact}
-            render={(routeProps: RouteChildrenProps<any>) => <route.component {...routeProps}/>}
-            />
-        ))}
-        </Switch>
-    )
-}
+        <UserContextProvider value={userContextValues}>
+            <Switch>
+                {routes.map((route, index) => {
+                    if (route.auth) {
+                        return (
+                            <Route
+                                path={route.path}
+                                exact={route.exact}
+                                key={index}
+                                render={(routeProps: RouteComponentProps) => 
+                                    <AuthRoute>
+                                        <route.component {...routeProps} />
+                                    </AuthRoute>
+                                }
+                            />
+                        );
+                    }
+
+                    return <Route 
+                    path={route.path} 
+                    exact={route.exact} 
+                    key={index} 
+                    render={(routeProps: RouteChildrenProps) => <route.component {...routeProps} />} 
+                    />;
+                })}
+            </Switch>
+        </UserContextProvider>
+    );
+};
 
 export default Application;
--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -1,5 +1,9 @@
 import firebase from 'firebase/compat/app';
 import { auth } from '../config/firebase';
+import IUser from '../interfaces/user';
+import axios from 'axios';
+import logging from '../config/logging';
+import config from '../config/config';
 
 export const SignInWithSocialMedia = (provider: firebase.auth.AuthProvider) =>
     new Promise<firebase.auth.UserCredential>((resolve, reject) => {
@@ -7,3 +11,45 @@ export const SignInWithSocialMedia = (provider: firebase.auth.AuthProvider) =>
             .then((result: any) => resolve(result))
             .catch((error: any) => reject(error));
     });
+
+export const Authenticate = async (uid: string, name: string, fire_token: string, callback: (error: string | null, user: IUser | null) => void) => {
+    try {
+        const response = await axios({
+            method: 'POST',
+            data: {
+                uid,
+                name
+            },
+            headers: { Authorization: `Bearer ${fire_token}` }
+        });
+        if (response.status === 200 || response.status === 201 || response.status === 304) {
+            callback(null, response.data.user);
+        } else {
+            logging.warn('unable to authenticate');
+            callback('unable to authenticate', null);
+        }
+    } catch (error) {
+        logging.error(error);
+        callback('unable to authenticate', null);
+    }
+};
+
+export const Validate = async (fire_token: string, callback: (error: string | null, user: IUser | null) => void) => {
+    try {
+        const response = await axios({
+            method: 'GET',
+            url: `${config.server.url}/user/validate`,
+            headers: { Authorization: `Bearer ${fire_token}` }
+        });
+        if (response.status === 200 || response.status === 304) {
+            logging.info('Successfully validated user');
+            callback(null, response.data.user);
+        } else {
+            logging.warn('unable to validate');
+            callback('unable to validate', null);
+        }
+    } catch (error) {
+        logging.error(error);
+        callback('unable to validate', null);
+    }
+};
